1010988307d8bf5261bd27d78f064f7782b695cc
Nora 3 <noraassaedi158@gmail.com>
2026-09-11 18:37:04 +0300

    Add DatabaseConnection class for MySQL connection

---
+++ DataBaseConnection.java
@@ -0,0 +1,17 @@
+package Model.database;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+public class DatabaseConnection {
+	
+		 {
+		        try {
+		        	Class.forName("com.mysql.cj.jdbc.Driver");
+		        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinema", "root", "EmanEman0987@");
+		        	System.out.println("connected");
+		        }
+		        catch(Exception e) {
+		        	System.out.println(e);
+		        }
+	      }
+}
