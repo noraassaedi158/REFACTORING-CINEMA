47ff8bc8eff0d6efae029a39321545c0bd1b38c1
Nora 3 <noraassaedi158@gmail.com>
2026-09-11 18:36:02 +0300

    Create main class to launch WelcomeView application

---
+++ main.java
@@ -0,0 +1,12 @@
+package Main;
+import javafx.application.*;
+import View.*;
+import Model.*;
+
+public class Main  {
+	public static void main(String[] args) {
+		Application.launch(WelcomeView.class,args);
+        
+	}
+
+}
