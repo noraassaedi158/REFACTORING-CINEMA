94eb6d428823f30f246bba9eb9c300335e0cef55
Nora 3 <noraassaedi158@gmail.com>
2026-09-11 18:34:53 +0300

    Add RollConnectionControl class with basic methods

---
+++ RollConnectionControl.java
@@ -0,0 +1,16 @@
+package Controller;
+import View.*;
+import Model.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.*;
+import javafx.stage.*;
+public class RollConnectionControl {
+ public static void chosenManeger(Stage s) {
+	LoginView log=new LoginView();
+	log.start(s);
+ }
+ public static void chosenCustomer() {
+	 
+ }
+}
