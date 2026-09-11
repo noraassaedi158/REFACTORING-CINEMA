18529f1e76742a254111df764fbb25750dfba5ea
Nora 3 <noraassaedi158@gmail.com>
2026-09-11 18:35:34 +0300

    Add WelcomeControl class for scene management

---
+++ WelcomeControl.java
@@ -0,0 +1,11 @@
+package Controller;
+import View.*;
+import Model.*;
+import javafx.event.ActionEvent;
+import javafx.scene.*;
+import javafx.stage.*;
+public class WelcomeControl {
+   public static void goRoles(Stage stage, Scene scene) {
+	  stage.setScene(scene);
+   }
+}
