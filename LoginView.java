0f509d2699be7dca42952d6b87b7ef4a6c7b7e91
Nora 3 <noraassaedi158@gmail.com>
2026-09-12 15:36:28 +0300

    Enhance LoginView with account creation options
    Added functionality for account creation in the login view.

--- LoginView.java
+++ LoginView.java
@@ -1,9 +1,12 @@
 package View;
+import Controller.AuthenticationControl;
+import Controller.RollConnectionControl;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.input.MouseEvent;
 
 public class LoginView extends Application{
 	public void start(Stage login) {
@@ -15,8 +18,29 @@ public class LoginView extends Application{
 		TextField pass=new TextField();
 		pass.setPromptText("Password");
 		HBox passBox= new HBox(password,pass);
-		VBox log = new VBox(userBox, passBox);
+		Label noAccount=new Label ("No Account? make one");
+		VBox log = new VBox(userBox, passBox, noAccount);
 		Scene allLogin= new Scene(log, 300, 300);
+		
+		Label newUserName=new Label("User: ");
+		TextField newUser=new TextField();
+		newUser.setPromptText("Username");
+		HBox newUserBox=new HBox(newUserName, newUser);
+		Label newPassword=new Label("Password: ");
+		TextField newPass=new TextField();
+		newPass.setPromptText("Password");
+		Label askAccount=new Label("Make account: ");
+		HBox newPassBox= new HBox( newPassword,newPass);
+		VBox newAccBox= new VBox(askAccount,newUserBox, newPassBox);
+		Scene makeAccount=new Scene(newAccBox, 300, 300);
+		
+		noAccount.setOnMouseClicked(click -> {RollConnectionControl.noAccount(login, makeAccount);} );
+		user.setOnAction(enter->{pass.requestFocus();});
+		pass.setOnAction(enter->{AuthenticationControl.accountCheck(user.getText(), pass.getText());});
+		newUser.setOnAction(enter->{newPass.requestFocus();});
+		newPass.setOnAction(enter->{AuthenticationControl.newAccountSave(newUser.getText(), newPass.getText());});
+		
+		
 		login.setScene(allLogin);
 		login.setTitle("Cinema");
 		login.show();
