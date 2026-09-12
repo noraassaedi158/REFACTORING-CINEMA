b60826d9e9eb6282b864b955e7b86f1dad4eb457
Nora 3 <noraassaedi158@gmail.com>
2026-09-12 15:51:20 +0300

    Refactor saveTheAccount method in Authentication class
    Refactor saveTheAccount method to use a class-level Statement and handle exceptions properly.

--- AuthenticationModel.java
+++ AuthenticationModel.java
@@ -2,9 +2,17 @@ package Model;
 import Model.database.*;
 import java.sql.*;
 public class Authentication {
+	static Statement state;
 	public static void saveTheAccount(String username, String password) {
-		Statement state= DatabaseConnection.getConnection().createStatement();
-		state.executeUpdate("INSERT INTO account (username) VALUES " + username );
+		try {
+		      state= DatabaseConnection.getConnection().createStatement();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			return;
+		}
+		state.excuteUpdate("INSERT INTO account(username, password) VALUES " + username + " " + password );
+		
 	}
 	public static void CheckTheAccount(String username, String password) {
 		
