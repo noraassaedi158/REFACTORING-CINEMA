cde77a533528a1ea2bcb283bee4c079d8e3d72f8
Nora 3 <noraassaedi158@gmail.com>
2026-09-12 15:40:37 +0300

    Enhance makeTables with try-catch for exceptions
    Added error handling to makeTables method.

--- DatabaseSetUp.java
+++ DatabaseSetUp.java
@@ -3,10 +3,15 @@ package Model.database;
 import java.sql.Statement;
 
 public class DatabaseSetUp {
-	public static void makeTables() {
+	public static void makeTables() {{
+		try {
 		Statement state= DatabaseConnection.getConnection().createStatement();
 		state.executeUpdate("CREATE TABLE account (username VARCHAR PRIMARY KEY, password VARCHAR NOT NULL UNIQUE)" );
 		
 	}
-
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
 }
