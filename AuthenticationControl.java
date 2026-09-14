a7d79957d610bb5fa60fa6c3cd86ab37e4f13253
Nora 3 <noraassaedi158@gmail.com>
2026-09-14 14:48:24 +0300

    Refactor authentication methods to use Stage parameter
    Refactored accountCheck and newAccountSave methods to include Stage parameter and update their functionality.

--- AuthenticationControl.java
+++ AuthenticationControl.java
@@ -1,11 +1,28 @@
 package Controller;
 import Model.*;
+import View.*;
+import javafx.stage.Stage;
 public class AuthenticationControl {
-	public static void accountCheck(String username, String password) {
-		Authentication.CheckTheAccount();
-	}
-	public static void newAccountSave(String username, String password) {
-		Authentication.saveTheAccount();
+	
+	public static void newAccountSave(String username, String password, Stage stage) {
+		if(Authentication.saveAccount(username, password)) {
+			LoginView login=new LoginView();
+			login.start(stage);
+		};
+		
 	}
 
+	public static void accountCheck(String username, String password, Stage stage) {
+		if (Authentication.CheckAccount(username, password)){
+			ManegmentView manege = new ManegmentView();
+			manege.start(stage);
+		}
+		else {
+			WelcomeView notLegit = new WelcomeView();
+			notLegit.start(stage);
+		}
+		 
+		
+	}
+	
 }
