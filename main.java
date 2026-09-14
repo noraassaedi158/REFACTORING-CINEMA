22b98fd11b007777b2d60fa35c7f21a064d49615
Nora 3 <noraassaedi158@gmail.com>
2026-09-14 14:50:23 +0300

    Add database connection and setup in main class

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
 package Main;
 import javafx.application.*;
 import View.*;
-import Model.*;
+import Model.database.*;
 
 public class Main  {
 	public static void main(String[] args) {
+		DatabaseConnection connect= new DatabaseConnection();
+		DatabaseSetUp setUp= new DatabaseSetUp();
 		Application.launch(WelcomeView.class,args);
         
 	}
