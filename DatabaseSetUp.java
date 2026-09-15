c8fdb736213966d80b4f382c09d0b0a1de407d13
Nora 3 <noraassaedi158@gmail.com>
2026-09-15 15:06:39 +0300

    Refactor DatabaseSetUp for improved table creation
    Refactored DatabaseSetUp class to improve table creation logic and added new tables.

--- DatabaseSetUp.java
+++ DatabaseSetUp.java
@@ -3,15 +3,19 @@ package Model.database;
 import java.sql.Statement;
 
 public class DatabaseSetUp {
-	public static void makeTables() {{
+	Statement state;
+	public DatabaseSetUp() {{
 		try {
-		Statement state= DatabaseConnection.getConnection().createStatement();
-		state.executeUpdate("CREATE TABLE account (username VARCHAR PRIMARY KEY, password VARCHAR NOT NULL UNIQUE)" );
+		    state= DatabaseConnection.getConnection().createStatement();
+		   state.executeUpdate("CREATE TABLE IF NOT EXISTS account (username VARCHAR(50) PRIMARY KEY, password VARCHAR(50) NOT NULL UNIQUE)" );
+		   state.executeUpdate("CREATE TABLE IF NOT EXISTS cinema (cinema_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50))");
+		   state.executeUpdate("CREATE TABLE IF NOT EXISTS room (id INT AUTO_INCREMENT FORIEGN KEY (cinema_id) REFERENCE cinema, )");
 		
 	}
 		catch(Exception e) {
 			System.out.println(e);
 		}
+		
 	}
 }
 }
