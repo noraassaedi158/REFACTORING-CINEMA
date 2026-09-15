8a67744d48d7661cc41eba15ca9f00450a1649c3
Nora 3 <noraassaedi158@gmail.com>
2026-09-15 15:07:37 +0300

    Add CinemaController class with initial methods

---
+++ CinemaController.java
@@ -0,0 +1,18 @@
+package Controller;
+import View.*;
+import Model.*;
+
+public class CinemaController {
+public static void CinemaInfo(String name, int num) {
+	
+}
+public static void RoomInfo() {
+	
+}
+public static void MovieInfo() {
+	
+}
+public static void DeleteMovie() {
+	
+}
+}
