f48661c0e0f583ad5b570cbbd48c496a095ef96d
Nora 3 <noraassaedi158@gmail.com>
2026-09-15 15:07:04 +0300

    Enhance ManegmentView with cinema and movie features
    Refactored the layout and added functionality for cinema and movie management.

--- ManegmentView.java
+++ ManegmentView.java
@@ -1,4 +1,5 @@
 package View;
+import Controller.*;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
@@ -10,14 +11,68 @@ import javafx.scene.input.*;
 import javafx.geometry.*;
 import javafx.collections.*;
 public class ManegmentView extends Application {
-	@Override
+	
     public void start(Stage manger) {
+		Label cinemaInfo = new Label("Cinema info");
+		Label cinemaName= new Label("Cinema name");
+		TextField Cname= new TextField();
+		Cname.setPromptText("name");
+		HBox name= new HBox(cinemaInfo ,cinemaName,Cname);
+		Label RoomNumber =new Label ("Rooms in Cinema");
+		Spinner<Integer> num=new Spinner<>(0, 110, 0);
+		HBox number=new HBox(RoomNumber, num);
+		Button enterCinInfo=new Button("Enter");
+		enterCinInfo.setOnAction(x->CinemaController.CinemaInfo(Cname.getText(), num.getValue()));
+		VBox cinInf=new VBox(name,number, enterCinInfo);
+	    BorderPane cinemaInfoBox = new BorderPane();
+	    cinemaInfoBox.setCenter(cinInf);
+	    cinemaInfoBox.setTop(cinemaInfo);
+	    
+	    
+	    Label roomNumber= new Label("Room");
+		Spinner<Integer> roomNum= new Spinner<>(0, num.getValue(), 0);
+		HBox numberRoom=new HBox(roomNumber, roomNum);
+		Label roomCapcity =new Label("room Capcity");
+		Spinner<Integer> currentRoomCapcity= new Spinner<>(0, 200, 0);
+		HBox capcity=new HBox(roomCapcity, currentRoomCapcity);
+		Button enterRoom = new Button("Enter");
+		enterRoom.setOnAction(x->CinemaController.RoomInfo());
+		VBox roomCap=new VBox(numberRoom,capcity, enterRoom );
+		BorderPane roomInfo =new BorderPane();
+		roomInfo.setCenter(roomCap);
 		
-        VBox rootLayout = new VBox(10); 
-        Scene page = new Scene(rootLayout, 400, 300);
-        primaryStage.setTitle("JavaFX Structure Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		
+	    Label setMovieScreening=new Label("Movie screening");
+	    Label movieName = new Label("Movie name");
+	    TextField setMovieName= new TextField();
+	    setMovieName.setPromptText("Movie name");
+	    HBox nameMovie=new HBox(movieName, setMovieName);
+	    Label movieGenre = new Label("Movie Genre");
+	    TextField setMovieGenre=new TextField();
+	    setMovieGenre.setPromptText("Genre");
+	    HBox genre=new HBox(movieGenre,setMovieGenre );
+	    Label ageRating = new Label("Movie age-rating");
+	    Spinner<String> rating= new Spinner<>(0, 0, 0);
+	    HBox ratingBox=new HBox(ageRating,rating);
+	    Label movieRoom = new Label("Room");
+	    Spinner<Integer> roomScreen= new Spinner<>(0, num.getValue(), 0);
+	    Button enterMovie=new Button("Add Movie");
+	    enterMovie.setOnAction(x->CinemaController.MovieInfo());
+	    HBox screenRoom= new HBox(movieRoom, roomScreen, enterMovie);
+	    Label deleteMovie= new Label("Delete movie");
+	    Spinner<String> delete= new Spinner<>(0, 0, 0);
+	    Button enterDelete= new Button("Delete Movie");
+	    enterDelete.setOnAction(x->CinemaController.DeleteMovie());
+	    HBox deleteBox= new HBox(deleteMovie, delete, enterDelete );
+	    VBox screenInfo= new VBox(setMovieScreening,nameMovie, genre,ratingBox, screenRoom, deleteBox);
+	    VBox allMovieInfo =new VBox(cinInf,roomCap, screenInfo);
+        Scene page = new Scene(allMovieInfo, 400, 300);
+        
+        
+        manger.setScene(page);
+        manger.setTitle("Cinema");
+        manger.show();
+        
     }
 
     public static void main(String[] args) {
