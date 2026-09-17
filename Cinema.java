6e671258b1dbd57c5f3573cc5fc4fb3186056158
Nora 3 <noraassaedi158@gmail.com>
2026-09-18 00:18:40 +0300

    Add Cinema class with room management functionality

---
+++ Cinema.java
@@ -0,0 +1,23 @@
+package Model;
+import Model.database.*;
+import java.util.ArrayList;
+public class Cinema {
+	private String name;
+	private ArrayList<Room> rooms = new ArrayList <>();
+
+public void name(String name, int num) {
+	this.name= name;
+	if (cinemaDataBase.CinemaExists(name.toLowerCase())) {
+		for (int i=1; i<=num; i++) {
+			rooms.add(new Room(i));
+		}
+	}
+	else {
+		cinemaDataBase.insertCinema(name.toLowerCase());
+	}
+}
+public void setCapcity( int num, int cap) {
+	rooms.get(num).setCap(cap);
+	
+}
+}
