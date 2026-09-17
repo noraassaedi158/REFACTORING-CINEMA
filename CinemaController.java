4ef0a5aed463fa7120cc93d226c391024c8dab21
Nora 3 <noraassaedi158@gmail.com>
2026-09-18 00:18:13 +0300

    Refactor CinemaController methods and add new functionality

--- CinemaController.java
+++ CinemaController.java
@@ -3,16 +3,21 @@ import View.*;
 import Model.*;
 
 public class CinemaController {
+	private static Cinema cin = new Cinema();
+
 public static void CinemaInfo(String name, int num) {
+	cin.name(name, num);
 	
 }
-public static void RoomInfo() {
+public static void RoomInfo(String name, int roomNum, int capRoom) {
+	cin.setCapcity(name, roomNum,capRoom);
 	
 }
-public static void MovieInfo() {
+public static void MovieInfo(String movieNames, String Genre, String age) {
 	
 }
 public static void DeleteMovie() {
 	
 }
-}
+public static void ScreenMovie() {
+	
