50a041c404672db0b352bb50bfe4c7526b7fb5c5
Nora 3 <noraassaedi158@gmail.com>
2026-09-22 18:33:09 +0300

    Refactor CinemaController methods and parameters

--- CinemaController.java
+++ CinemaController.java
@@ -1,27 +1,40 @@
 package Controller;
 import View.*;
+
+import java.util.ArrayList;
+
 import Model.*;
 
 public class CinemaController {
 	private static Cinema cin = new Cinema();
 	private static Movie movie= new Movie();
-
+	private static ageRating age[];
+	
+public static int getRoomNumber() {
+	return cin.getRooms().size();
+}
+public static ageRating[] getRating() {
+	age=ageRating.values();
+	return age;
+}
 public static void CinemaInfo(String name, int num) { 
 	cin.name(name, num);
 	
 }
-public static void RoomInfo( int roomNum, int capRoom) {
+public static void RoomInfo(int roomNum, int capRoom) {
 	cin.setCapcity( roomNum,capRoom);
 	
 }
-public static void MovieInfo( String movieNames, String Genre, String age) {
-	movie.setMovieInfo(cin, movieNames, Genre, age );
+public static void MovieInfo( String movieNames, String Genre, ageRating age) {
+	String ages= age.name();
+	movie.setMovieInfo(cin, movieNames, Genre, ages );
 	
 }
-public static void DeleteMovie() {
+public static ArrayList movies() {
 	
 }
-public static void ScreenMovie() {
+public static void DeleteMovie(String name, String cinemaName) {
+	movie.delete(name, cinemaName);
 	
 }
 }
