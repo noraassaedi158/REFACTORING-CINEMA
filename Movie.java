9170bb16f3ca0cdcbeff00eae565e47bddbe9f2c
Nora 3 <noraassaedi158@gmail.com>
2026-09-22 18:32:00 +0300

    Enhance Movie class with additional properties and methods
    Added properties for genre, age, and cinema name. Implemented constructor and methods for setting movie info and deleting movies.

--- Movie.java
+++ Movie.java
@@ -1,12 +1,21 @@
 package Model;
 
+import java.util.ArrayList;
+
 import Model.database.cinemaDataBase;
 
 public class Movie {
-	
 	private String title;
-	Room room;
-	double price;
+	private Room room;
+	private String Genre;
+	private String age;
+	private double price;
+	private String cinemaName;
+    public Movie(String cinemaName,String movieNames, String Genre, String age ) {
+    	title=movieNames;
+		this.Genre=Genre;
+		this.age=age;
+    }
 	
 	public Movie() {
 		title=null;
@@ -14,6 +23,15 @@ public class Movie {
 	}
 	public void setMovieInfo(Cinema cin, String movieNames, String Genre, String age) {
 		cinemaDataBase.createMovie( cin, movieNames, Genre, age);
+		title=movieNames;
+		this.Genre=Genre;
+		this.age=age;
+	}
+	public void delete(String name, String cinemaMovie) {
+		cinemaDataBase.deleteMovie(name, cinemaMovie);
+	}
+	public ArrayList movies(Cinema cin) {
+		
 	}
 
 }
