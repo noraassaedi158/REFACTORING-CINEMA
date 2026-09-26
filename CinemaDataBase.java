bf82ede1621b2f4fb602b14a193286bf083d7537
Nora 3 <noraassaedi158@gmail.com>
2026-09-26 22:27:27 +0300

    Enhance cinema methods with username parameter
    Updated cinema methods to include username for user-specific operations.

--- CinemaDataBase.java
+++ CinemaDataBase.java
@@ -8,28 +8,40 @@ import Model.Movie;
 import Model.Room;
 public class cinemaDataBase {
 	
-	public static Boolean CinemaExists(String name) {
-		try(PreparedStatement exist = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name = (?)")){
+	public static Boolean CinemaExists(String name, String username) {
+		try(PreparedStatement Userexist = DatabaseConnection.getConnection().prepareStatement("SELECT user_id FROM account WHERE username = (?) ")){
+			Userexist.setString(1, username);
+			ResultSet UseExist=Userexist.executeQuery();
+			if (UseExist.next()) {
+		try(PreparedStatement exist = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name = (?) and user_id =(?)")){
 			exist.setString(1, name);
+			exist.setInt(2,UseExist.getInt("user_id"));
 			ResultSet cinExist=exist.executeQuery();
 			if (cinExist.next()) {
 				return true;
 			}
 			else {
 				return false;
-			}
+			}}}
+		return false;
 		}
 		catch(Exception e) {
 			return false;
 		}
 	}
-	public static Boolean insertCinema(String name) {
-		try(PreparedStatement cinMake = DatabaseConnection.getConnection().prepareStatement("INSERT INTO cinema (name) VALUES (?)")){
+	public static Boolean insertCinema(String name,String username) {
+		try(PreparedStatement Userexist = DatabaseConnection.getConnection().prepareStatement("SELECT user_id FROM account WHERE username = (?) ")){
+			Userexist.setString(1, username);
+			ResultSet UseExist=Userexist.executeQuery();
+			if (UseExist.next()) {
+		try(PreparedStatement cinMake = DatabaseConnection.getConnection().prepareStatement("INSERT INTO cinema (name,user_id ) VALUES (?, ?)")){
 			cinMake.setString(1, name);
+			cinMake.setInt(2,UseExist.getInt("user_id"));
 			cinMake.executeUpdate();
 			return true;
 			
-	}
+	}}
+		return false;}
 		catch(Exception e) {
 			return false;
 		}
@@ -38,32 +50,76 @@ public class cinemaDataBase {
 	
 	public static Boolean makeRoom(Cinema cin) {
 		String name=cin.getName();
-		ArrayList<Room> rooms=cin.getRooms();
+		ArrayList<Room> rooms=cin.getRoomsData();
 		
 		try(PreparedStatement getID = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name= (?)")){
 			getID.setString(1, name);
 			ResultSet cinemaExist= getID.executeQuery();
 			if (cinemaExist.next()) {
 			for(int i=0; i<rooms.size(); i++) {
-			try(PreparedStatement makeRoom = DatabaseConnection.getConnection().prepareStatement("INSERT INTO room (cinema_id, room_num, capacity) VALUES (?, ?, ?)")){
+			try(PreparedStatement makeRoom = DatabaseConnection.getConnection().prepareStatement("INSERT INTO room (cinema_id, room_num) VALUES (?, ?)")){
 				makeRoom.setInt(1, cinemaExist.getInt( "cinema_id"));
 				makeRoom.setInt(2, rooms.get(i).getRoomNumber() );
-				makeRoom.setInt(3,rooms.get(i).getCap());
 				makeRoom.executeUpdate();
 				
 			}
-			return true;
+			
 			}
 			return true;
 		}
 		else {
-			System.out.println("idk for now");
+			System.out.println("idk forww now");
 			return false;
 		}
 			}
 		catch(Exception e) {
 			return false;
 		}
+	}
+	public static Boolean fillRoom(Cinema cin, int num, int cap) {
+		String name=cin.getName();
+		try(PreparedStatement getID = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name= (?)")){
+			getID.setString(1, name);
+			ResultSet cinemaExist= getID.executeQuery();
+			if (cinemaExist.next()) {
+			try(PreparedStatement makeRoom = DatabaseConnection.getConnection().prepareStatement("UPDATE room SET capacity = (?)  WHERE cinema_id =(?) AND room_num =(?)  ")){
+				makeRoom.setInt(1, cap );
+				makeRoom.setInt(2, cinemaExist.getInt( "cinema_id"));
+				makeRoom.setInt(3, num );
+				makeRoom.executeUpdate();
+				
+			}
+			return true;
+			}
+			else {
+				System.out.println("idk for now");
+				return false;
+			}
+		}
+		catch(Exception e) {
+			return false;
+		}
+			}
+	
+	public static ArrayList<Integer> getRooms(String cin) {
+		ArrayList<Integer> rooms= new ArrayList<>();
+		
+		try(PreparedStatement getID = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name= (?)")){
+			getID.setString(1, cin);
+			ResultSet cinemaExist= getID.executeQuery();
+			if (cinemaExist.next()) {
+				try(PreparedStatement roomsID = DatabaseConnection.getConnection().prepareStatement("SELECT room_ID FROM room WHERE cinema_id= (?)")){
+					roomsID.setInt(1, cinemaExist.getInt( "cinema_id"));
+					ResultSet allRooms =roomsID.executeQuery();
+					while(allRooms.next()) {
+						int room= allRooms.getInt("room_ID");
+						rooms.add(room);
+					}
+			} } }
+		catch(Exception e) {
+			return rooms;
+		}
+		return rooms;
 	}
 	public static Boolean createMovie(Cinema cin, String name, String genre, String age) {
 		String Cname=cin.getName();
@@ -114,27 +170,31 @@ public class cinemaDataBase {
 }
 		return false;}
 	
-	public static ArrayList getMovies(Cinema cin) {
+	
+	public static ArrayList<Movie> getMovies(String cin) {
 		ArrayList<Movie> movies= new ArrayList<>();
-		String Cname=cin.getName();
 		try(PreparedStatement getID = DatabaseConnection.getConnection().prepareStatement("SELECT cinema_id FROM cinema WHERE name= (?)")){
-			getID.setString(1, Cname);
+			getID.setString(1, cin);
 			ResultSet cinemaExist= getID.executeQuery();
 			if (cinemaExist.next()) {
-				try(PreparedStatement movies = DatabaseConnection.getConnection().prepareStatement("SELECT movie_name FROM movie WHERE cinema_id= (?)"){
-					movies.setInt(1, cinemaExist.getInt( "cinema_id"));
-					ResultSet allMovies =movies.executeQuery();
+				try(PreparedStatement allmovies = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM movie WHERE cinema_id= (?)")){
+					allmovies.setInt(1, cinemaExist.getInt( "cinema_id"));
+					ResultSet allMovies =allmovies.executeQuery();
 					while(allMovies.next()) {
-						String cinemaName=Cname;
 						String name=allMovies.getString("movie_name");
-						String genre= allMovies.getString("genre");
-						String age=allMovies.getString("age");
-						Movie movie= new Movie(Cname,name,genre, age);
+						String genre= allMovies.getString("movie_genre");
+						String age=allMovies.getString("age_rating");
+						Movie movie= new Movie(name,genre, age);
 						movies.add(movie);
 					}
+					return movies;
 				}
+				
 			}
 		
 	}
+		catch(Exception e) {
+			return movies;
+		} return movies;
 	}   }
 
